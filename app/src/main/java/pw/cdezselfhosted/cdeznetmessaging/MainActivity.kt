34ac6b81a2098096ef26d0e900bbe0162f9c3cc4
cmd644 <cmd644@nau.edu>
2025-04-05 12:41:40 -0700

    Finished part 1

--- app/src/main/java/pw/cdezselfhosted/cdeznetmessaging/MainActivity.kt
+++ app/src/main/java/pw/cdezselfhosted/cdeznetmessaging/MainActivity.kt
@@ -1,11 +1,12 @@
 package pw.cdezselfhosted.cdeznetmessaging
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -15,6 +16,10 @@ import androidx.appcompat.widget.Toolbar
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
+
+    private var serverAddress: String = "http://localhost:5000"
+    private var username: String = "anonymous"
+    private var chatroom: String = "default"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,18 +47,30 @@ class MainActivity : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        // Set default values
-        val serverAddress = navigationView.menu.findItem(R.id.nav_server_address).actionView as EditText?
-        val username = navigationView.menu.findItem(R.id.nav_username).actionView as EditText?
-        val chatroom = navigationView.menu.findItem(R.id.nav_chatroom).actionView as EditText?
-
-        serverAddress?.setText("http://localhost:5000")
-        username?.setText("anonymous")
-        chatroom?.setText("default")
-
         // Handle menu item clicks
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
+                R.id.nav_server_address -> {
+                    showInputDialog("Server Address", serverAddress) { newValue ->
+                        serverAddress = newValue
+                        Toast.makeText(this, "Server Address updated to $serverAddress", Toast.LENGTH_SHORT).show()
+                    }
+                    true
+                }
+                R.id.nav_username -> {
+                    showInputDialog("Username", username) { newValue ->
+                        username = newValue
+                        Toast.makeText(this, "Username updated to $username", Toast.LENGTH_SHORT).show()
+                    }
+                    true
+                }
+                R.id.nav_chatroom -> {
+                    showInputDialog("Chatroom", chatroom) { newValue ->
+                        chatroom = newValue
+                        Toast.makeText(this, "Chatroom updated to $chatroom", Toast.LENGTH_SHORT).show()
+                    }
+                    true
+                }
                 R.id.nav_theme -> {
                     Toast.makeText(this, "Theme changed!", Toast.LENGTH_SHORT).show()
                     true
@@ -65,5 +82,30 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    /**
+     * Show an input dialog to update a value.
+     * @param title The title of the dialog.
+     * @param currentValue The current value to display in the input field.
+     * @param onValueChanged Callback to handle the updated value.
+     */
+    private fun showInputDialog(title: String, currentValue: String, onValueChanged: (String) -> Unit) {
+        val input = EditText(this)
+        input.setText(currentValue)
+
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(input)
+            .setPositiveButton("OK") { _, _ ->
+                val newValue = input.text.toString()
+                if (newValue.isNotEmpty()) {
+                    onValueChanged(newValue)
+                } else {
+                    Toast.makeText(this, "$title cannot be empty", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 }
